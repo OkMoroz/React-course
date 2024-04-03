6183923c4becd54586a5d264f26e7e6017f04385
OkMoroz <k.moroz.mail@gmail.com>
2024-04-03 20:04:17 +0300

    props

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -71,11 +71,33 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato,mozarella,spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizzas/funghi.jpg"
+      />
     </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.s}</p>
+        <span>{props.price + 3}</span>
+      </div>
+    </div>
   );
 }
 function Footer() {
@@ -89,15 +111,7 @@ function Footer() {
 
   return <footer className="footer"> ${hour}. We're currently open!</footer>;
 }
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
