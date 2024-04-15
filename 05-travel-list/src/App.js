b9d1837c1096ca18f786f5120852c2391eb78ca4
OkMoroz <k.moroz.mail@gmail.com>
2024-04-15 14:41:06 +0300

    onChange

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,4 +1,6 @@
 import { React } from "react";
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -19,6 +21,9 @@ function Logo() {
   return <h1>Far Away </h1>;
 }
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState("1");
+
   function handleSubmit(e) {
     e.preventDefault();
     console.log(e);
@@ -26,14 +31,28 @@ function Form() {
   return (
     <div className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setQuantity(Number(e.target.value));
+        }}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          // console.log(e.target.value);
+          setDescription(e.target.value);
+        }}
+      ></input>
       <button>Add</button>
     </div>
   );
