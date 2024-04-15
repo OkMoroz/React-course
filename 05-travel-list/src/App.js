c239ff4dcd22df8cea675e3c07d11e049e96ada5
OkMoroz <k.moroz.mail@gmail.com>
2024-04-15 15:14:06 +0300

    newItem

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -22,21 +22,26 @@ function Logo() {
 }
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState("1");
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log(e);
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   }
+
   return (
-    <div className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip?</h3>
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your 😍 trip?</h3>
       <select
         value={quantity}
-        onChange={(e) => {
-          // console.log(e.target.value);
-          setQuantity(Number(e.target.value));
-        }}
+        onChange={(e) => setQuantity(Number(e.target.value))}
       >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
@@ -48,13 +53,10 @@ function Form() {
         type="text"
         placeholder="Item..."
         value={description}
-        onChange={(e) => {
-          // console.log(e.target.value);
-          setDescription(e.target.value);
-        }}
-      ></input>
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
-    </div>
+    </form>
   );
 }
 
