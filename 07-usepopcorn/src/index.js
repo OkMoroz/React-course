360eb7091ae41195b5a5fb919092a25b505b7586
OkMoroz <k.moroz.mail@gmail.com>
2024-05-06 11:56:21 +0300

    PropTypes

--- 07-usepopcorn/src/index.js
+++ 07-usepopcorn/src/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./StarRating";
-// import "./index.css";
-// import App from "./App";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,5 +21,6 @@ root.render(
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
