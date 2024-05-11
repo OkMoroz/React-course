2b7177f92f7d6941930fb7f260177dcaa21d6b93
OkMoroz <k.moroz.mail@gmail.com>
2024-05-11 19:07:52 +0300

    useEffect

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -54,9 +54,12 @@ export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   const [watched, setWatched] = useState(tempWatchedData);
 
-  fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then((res) =>
-    res.json().then((data) => console.log(data))
-  );
+  useEffect(function () {
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`).then((res) =>
+      res.json().then((data) => setMovies(data.Search))
+    );
+  },[]);
+
   return (
     <>
       <NavBar>
