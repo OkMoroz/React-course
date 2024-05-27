2ba4119a51fb2e96594edddd9a64bd71666bb910
OkMoroz <k.moroz.mail@gmail.com>
2024-05-27 13:47:26 +0300

    search

--- 07-usepopcorn/src/App.js
+++ 07-usepopcorn/src/App.js
@@ -103,13 +103,11 @@ function Logo() {
 }
 
 function Search({ query, setQuery }) {
-  const inputEl = useRef(null);
-
-  useKey("Enter", function () {
-    if (document.activeElement === inputEl.current) return;
-    inputEl.current.focus();
-    setQuery("");
-  });
+  useEffect(function () {
+    const el = document.querySelector(".search");
+    console.log(el);
+    el.focus();
+  }, []);
 
   return (
     <input
@@ -118,7 +116,6 @@ function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
-      ref={inputEl}
     />
   );
 }
