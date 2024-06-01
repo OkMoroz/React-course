720a84622ab8b87dfac29b4f08cc9ffa58304315
OkMoroz <k.moroz.mail@gmail.com>
2024-06-01 20:45:01 +0300

    getWeather

--- 09-classy-weather/src/Counter.js
+++ 09-classy-weather/src/Counter.js
@@ -8,22 +8,29 @@ class Counter extends React.Component {
     this.handleDecrement = this.handleDecrement.bind(this);
     this.handleIncrement = this.handleIncrement.bind(this);
   }
+
   handleDecrement() {
-    // console.log(this)
     this.setState((curState) => {
       return { count: curState.count - 1 };
     });
   }
+
   handleIncrement() {
     this.setState((curState) => {
       return { count: curState.count + 1 };
     });
   }
+
   render() {
+    const date = new Date("june 21 2027");
+    date.setDate(date.getDate() + this.state.count);
+
     return (
       <div>
         <button onClick={this.handleDecrement}>-</button>
-        <span>{this.state.count}</span>
+        <span>
+          {date.toDateString()} [{this.state.count}]
+        </span>
         <button onClick={this.handleIncrement}>+</button>
       </div>
     );
