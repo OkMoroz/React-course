34f2a7970b54d3f30705c7fd6282d92145fdde8d
OkMoroz <k.moroz.mail@gmail.com>
2024-06-17 10:53:06 +0300

    fixed timer

--- 10-react-quiz/src/components/Timer.js
+++ 10-react-quiz/src/components/Timer.js
@@ -1,27 +1,16 @@
 import { useEffect } from "react";
 
 function Timer({ dispatch, secondsRemaining }) {
-  const mins = Math.floor(secondsRemaining / 60);
-  const seconds = secondsRemaining % 60;
-
   useEffect(
     function () {
-      const id = setInterval(function () {
+      setInterval(function () {
         dispatch({ type: "tick" });
       }, 1000);
-
-      return () => clearInterval(id);
     },
     [dispatch]
   );
 
-  return (
-    <div className="timer">
-      {mins < 10 && "0"}
-      {mins}:{seconds < 10 && "0"}
-      {seconds}
-    </div>
-  );
+  return <div className="timer">{secondsRemaining}</div>;
 }
 
 export default Timer;
