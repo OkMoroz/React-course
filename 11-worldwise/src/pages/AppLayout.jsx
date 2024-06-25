fa9a5d6b11442dda6ea058dc404c5e0652359773
OkMoroz <k.moroz.mail@gmail.com>
2024-06-25 13:28:52 +0300

    added images and styles

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -1,8 +1,11 @@
 import Sidebar from "../components/Sidebar";
+import Map from "../components/Map";
+import styles from "./AppLayout.module.css";
 function AppLayout() {
   return (
     <div>
       <Sidebar />
+      <Map />
     </div>
   );
 }
