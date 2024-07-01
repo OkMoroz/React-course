18cb0b62ad0d33ca7340d7ea9fd26706765b7374
OkMoroz <k.moroz.mail@gmail.com>
2024-07-01 19:03:00 +0300

    fixed

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import CityList from "./components/CityList";
+const BASE_URL = "http://localhost:9000";
 function App() {
+  const [cities, setCities] = useState({});
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error fetching cities");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
@@ -13,8 +33,8 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="app" element={<AppLayout />}>
-          <Route index element={<p>List of cities</p>} />
-          <Route path="cities" element={<p>List of cities</p>} />
+          <Route index element={<CityList />} />
+          <Route path="cities" element={<CityList />} />
           <Route path="countries" element={<p>Countries</p>} />
           <Route path="form" element={<p>Form</p>} />
         </Route>
