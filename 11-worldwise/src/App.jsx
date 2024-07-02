766bde3cc36c3fb97dbc61a3d6f93a71bbbe65ae
OkMoroz <k.moroz.mail@gmail.com>
2024-07-02 19:38:36 +0300

    added city

--- 11-worldwise/src/App.jsx
+++ 11-worldwise/src/App.jsx
@@ -8,6 +8,8 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
+import City from "./components/City";
+import Login from "./components/Login";
 
 const BASE_URL = "http://localhost:9000";
 function App() {
@@ -29,29 +31,32 @@ function App() {
     }
     fetchCities();
   }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
-        <Route path="app" element={<AppLayout />}>
-          <Route
-            index
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          <Route
-            path="cities"
-            element={<CityList cities={cities} isLoading={isLoading} />}
-          />
-          <Route
-            path="countries"
-            element={<CountryList />}
-            cities={cities}
-            isLoading={isLoading}
-          />
-          <Route path="form" element={<p>Form</p>} />
-        </Route>
+        <Route path="app" element={<AppLayout />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route
+          index
+          element={<CityList cities={cities} isLoading={isLoading} />}
+        />
+        <Route
+          path="cities"
+          element={<CityList cities={cities} isLoading={isLoading} />}
+        />
+        <Route path="cities/:id" element={<City />} />
+        <Route
+          path="countries"
+          element={<CountryList />}
+          cities={cities}
+          isLoading={isLoading}
+        />
+        <Route path="form" element={<p>Form</p>} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
